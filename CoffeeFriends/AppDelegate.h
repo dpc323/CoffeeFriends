f2068835c7fe593f5267b7a9c3509089591e177b
Ben <dpc323@126.com>
2017-01-02 15:25:38 +0800

    Initial Commit

---
+++ CoffeeFriends/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoffeeFriends
+//
+//  Created by hanarobot on 17/1/2.
+//  Copyright © 2017年 CFC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
